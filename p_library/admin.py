720a5824000c457b49e4320c4c450e2f43e5360f
NIKOLAY VETROV <nvetrov@gmail.com>
2020-01-14 20:37:32 +0300

    init

---
+++ p_library/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from p_library.models import Book, Author, Publisher
+
+
+@admin.register(Author)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'birth_year', 'country')
+
+
+@admin.register(Publisher)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    @staticmethod
+    def author_full_name(obj):
+        return obj.author.full_name
+
+    list_display = ('title', 'author_full_name',)
+    fields = ('ISBN', 'title', 'description', 'year_release', 'author', 'Publisher_id', 'copy_count', 'price')
+    '''
+    Для этого мы перечисляем в свойстве fields класса BookAdmin все поля, которые разрешено редактировать
+    В результате мы увидим, что поле copy_count исчезло из панели редактирования и создания. Оно теперь будет принимать
+    default-значение, указанное в модели Book. Его редактирование теперь будет доступно только из shell.
+    
+    class AuthorAdmin(admin.ModelAdmin):
+    exclude = ('birth_date',)
+    
+    '''
---
+++ p_library/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Author(models.Model):
+    full_name = models.TextField()
+    birth_year = models.SmallIntegerField()
+    country = models.CharField(max_length=2)
+
+
+class Publisher(models.Model):
+    name = models.TextField(blank=True, null=True)
+
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    title = models.TextField()
+    description = models.TextField()
+    year_release = models.SmallIntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    copy_count = models.SmallIntegerField()
+    Publisher_id = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    price = models.FloatField()
